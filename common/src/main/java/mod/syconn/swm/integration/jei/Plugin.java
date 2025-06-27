13d895623ccaef38951ca1cba3e4bed7cdd69f84
Syconn <49461127+Syconn@users.noreply.github.com>
2025-06-26 20:58:26 -0400

    Assets

---
+++ common/src/main/java/mod/syconn/swm/integration/jei/Plugin.java
@@ -0,0 +1,53 @@
+package mod.syconn.swm.integration.jei;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.registration.ISubtypeRegistration;
+import mod.syconn.swm.core.ModBlocks;
+import mod.syconn.swm.core.ModItems;
+import mod.syconn.swm.core.ModRecipes;
+import mod.syconn.swm.features.lightsaber.data.LightsaberTag;
+import mod.syconn.swm.integration.jei.category.LightsaberAssemblerCategory;
+import mod.syconn.swm.util.Constants;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+@JeiPlugin
+public class Plugin implements IModPlugin {
+
+    @Override
+    public @NotNull ResourceLocation getPluginUid() {
+        return Constants.withId("crafting");
+    }
+
+    @Override
+    public void registerItemSubtypes(ISubtypeRegistration registration) {
+        registration.registerSubtypeInterpreter(ModItems.LIGHTSABER.get(), (stack, context) -> String.valueOf(LightsaberTag.getOrCreate(stack).model));
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        IGuiHelper helper = registration.getJeiHelpers().getGuiHelper();
+        registration.addRecipeCategories(new LightsaberAssemblerCategory(helper));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        ClientLevel world = Objects.requireNonNull(Minecraft.getInstance().level);
+        registration.addRecipes(LightsaberAssemblerCategory.LIGHTSABER, world.getRecipeManager().getAllRecipesFor(ModRecipes.LIGHTSABER.get()));
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.LIGHTSABER_WORKBENCH.get()), LightsaberAssemblerCategory.LIGHTSABER);
+    }
+}
