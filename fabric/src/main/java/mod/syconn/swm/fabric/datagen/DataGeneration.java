5db66b068fb04d102424be7a06b252fd471b1ac1
Syconn <49461127+Syconn@users.noreply.github.com>
2025-06-28 04:25:19 -0400

    Recipe Files Updated

--- fabric/src/main/java/mod/syconn/swm/fabric/datagen/DataGeneration.java
+++ fabric/src/main/java/mod/syconn/swm/fabric/datagen/DataGeneration.java
@@ -3,7 +3,7 @@ package mod.syconn.swm.fabric.datagen;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class DataGeneration implements DataGeneratorEntrypoint {
+public class DataGeneration implements DataGeneratorEntrypoint { // TODO TEST THIS RUNS
 
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator generator) {
