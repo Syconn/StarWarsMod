3adc893f8a9bb622df282e00377e5e85af033417
Syconn <49461127+Syconn@users.noreply.github.com>
2025-07-02 03:46:06 -0400

    NBT Code for Network

--- common/src/main/java/mod/syconn/swm/server/savedata/HologramNetwork.java
+++ common/src/main/java/mod/syconn/swm/server/savedata/HologramNetwork.java
@@ -1,8 +1,10 @@
 package mod.syconn.swm.server.savedata;
 
 import mod.syconn.swm.utils.block.WorldPos;
+import mod.syconn.swm.utils.nbt.NbtTools;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.saveddata.SavedData;
 import org.jetbrains.annotations.NotNull;
@@ -22,14 +24,27 @@ public class HologramNetwork extends SavedData {
         var map = new ListTag();
         for (var entry : this.CALLS.entrySet()) {
             var tag = new CompoundTag();
-
+            tag.putUUID("uuid", entry.getKey());
+            tag.put("calls", NbtTools.putArray(entry.getValue(), Call::save));
+            map.add(tag);
         }
         compoundTag.put(tagID, map);
         return compoundTag;
     }
 
-    public static HologramNetwork load(CompoundTag pTag) {
-        return create();
+    public void read(CompoundTag tag) {
+        if(tag.contains(tagID, Tag.TAG_LIST)) {
+            tag.getList(tagID, Tag.TAG_COMPOUND).forEach(nbt -> {
+                var compoundTag = (CompoundTag) nbt;
+                CALLS.put(compoundTag.getUUID("uuid"), NbtTools.getArray(tag.getCompound("calls"), Call::from));
+            });
+        }
+    }
+
+    public static HologramNetwork load(CompoundTag tag) {
+        var network = create();
+        network.read(tag);
+        return network;
     }
 
     private static HologramNetwork create() {
@@ -42,23 +57,28 @@ public class HologramNetwork extends SavedData {
 
     public record Call(Caller owner, List<Caller> participants) {
         public static Call from(CompoundTag tag) {
-            return new Call();
+            return new Call(Caller.from(tag.getCompound("owner")), NbtTools.getArray(tag.getCompound("participants"), Caller::from));
         }
 
         public CompoundTag save() {
             var tag = new CompoundTag();
+            tag.put("owner", owner.save());
+            tag.put("participants", NbtTools.putArray(participants, Caller::save));
             return  tag;
         }
     }
 
     public record Caller(UUID uuid, Optional<WorldPos> location, boolean handheld) {
         public static Caller from(CompoundTag tag) {
-            return new Caller();
+            return new Caller(tag.getUUID("uuid"), NbtTools.getOptional(tag.getCompound("location"), WorldPos::from), tag.getBoolean("handheld"));
         }
 
+        @SuppressWarnings("OptionalGetWithoutIsPresent")
         public CompoundTag save() {
             var tag = new CompoundTag();
-            tag.put("uuid", )
+            tag.putUUID("uuid", this.uuid);
+            tag.put("location", NbtTools.putOptional(location, t -> t.get().save()));
+            tag.putBoolean("handheld", this.handheld);
             return  tag;
         }
     }
