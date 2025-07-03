6be7b4927cad4de20f9d752658f8a5bddd9aa008
Syconn <49461127+Syconn@users.noreply.github.com>
2025-07-02 23:05:45 -0400

    Fixed Scaled Button hover logic

--- common/src/main/java/mod/syconn/swm/client/screen/components/CallButton.java
+++ common/src/main/java/mod/syconn/swm/client/screen/components/CallButton.java
@@ -11,7 +11,7 @@ public class CallButton extends ExpandedButton {
     private final Type type;
 
     public CallButton(int xPos, int yPos, Type type, OnPress handler) {
-        super(xPos, yPos, 18, 18, Component.empty(), handler);
+        super(xPos, yPos, (int)(20 * 0.80f), (int)(20 * 0.80f), Component.empty(), handler);
         this.type = type;
     }
 
@@ -21,8 +21,8 @@ public class CallButton extends ExpandedButton {
 
         var uX = type == Type.START ? 0 : 20;
         var uY = isHovered ? 58 : 38;
-        guiGraphics.pose().scale(0.75f, 0.75f, 0.75f);
-        guiGraphics.blit(HOLOGRAM_SCREEN, this.getX() * 4 / 3, this.getY() * 4 / 3, uX, uY, width, height);
+        guiGraphics.pose().scale(0.80f, 0.80f, 0.80f);
+        guiGraphics.blit(HOLOGRAM_SCREEN, this.getX() * 5 / 4, this.getY() * 5 / 4, uX, uY, 20, 20);
 
         guiGraphics.pose().popPose();
     }
