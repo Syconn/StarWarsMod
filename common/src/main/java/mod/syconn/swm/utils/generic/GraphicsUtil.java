23b12adadde2a095bfe9086b7d408d3498961f9a
Syconn <49461127+Syconn@users.noreply.github.com>
2025-08-01 02:05:41 -0400

    Table Render Fixed

--- common/src/main/java/mod/syconn/swm/utils/generic/GraphicsUtil.java
+++ common/src/main/java/mod/syconn/swm/utils/generic/GraphicsUtil.java
@@ -6,6 +6,7 @@ import com.mojang.math.Axis;
 import dev.architectury.utils.GameInstance;
 import mod.syconn.swm.features.lightsaber.item.LightsaberItem;
 import mod.syconn.swm.features.lightsaber.server.data.LightsaberTag;
+import mod.syconn.swm.utils.client.NodeVec3;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.GameRenderer;
@@ -87,12 +88,12 @@ public class GraphicsUtil {
         final var minecraft = GameInstance.getClient();
 
         if (minecraft != null) {
-            final var emitterPos = LightsaberTag.getOrCreate(stack).getPrimaryBlade() != null ? LightsaberTag.getOrCreate(stack).getPrimaryBlade().emitterPos : null;
+            final var emitterPos = LightsaberTag.getOrCreate(stack).getPrimaryBlade() != null ? LightsaberTag.getOrCreate(stack).getPrimaryBlade().emitterPos : new NodeVec3();
             final var scale = 100f;
 
             guiGraphics.pose().pushPose();
             guiGraphics.pose().translate(x, y, 50.0);
-            if (emitterPos != null) guiGraphics.pose().translate((emitterPos.y() - LightsaberTag.getOrCreate(stack).hiltLength() / 2) * scale, 0, 0);
+            guiGraphics.pose().translate((emitterPos.y() - LightsaberTag.getOrCreate(stack).hiltLength() / 2) * scale, 0, 0);
             guiGraphics.pose().mulPose(Axis.ZP.rotationDegrees(-90f));
             guiGraphics.pose().mulPose(Axis.YP.rotationDegrees(-rotation));
             guiGraphics.pose().scale(scale, scale, scale);
@@ -114,16 +115,16 @@ public class GraphicsUtil {
         final var minecraft = GameInstance.getClient();
 
         if (minecraft != null) {
-            final var emitterPos = LightsaberTag.getOrCreate(stack).getPrimaryBlade() != null ? LightsaberTag.getOrCreate(stack).getPrimaryBlade().emitterPos : null;
-            final var scale = 100;
+            final var emitterPos = LightsaberTag.getOrCreate(stack).getPrimaryBlade() != null ? LightsaberTag.getOrCreate(stack).getPrimaryBlade().emitterPos : new NodeVec3();
             final var hiltLength = LightsaberTag.getOrCreate(stack).hiltLength();
+            final var scale = 100;
 
             guiGraphics.pose().pushPose();
             guiGraphics.pose().translate(x, y, 50.0);
-            if (emitterPos != null) guiGraphics.pose().translate((emitterPos.y() - hiltLength) * scale - 1, 0, 0);
+            guiGraphics.pose().translate((emitterPos.y() - hiltLength) * scale - 1, 0, 0);
             guiGraphics.pose().mulPose(Axis.ZP.rotationDegrees(-90f));
             guiGraphics.pose().mulPose(Axis.YP.rotationDegrees(-yRot));
-            if (targetBlade != -1 && emitterPos != null && LightsaberTag.getOrCreate(stack).blades.get(targetBlade) != null) guiGraphics.pose()
+            if (targetBlade != -1 && LightsaberTag.getOrCreate(stack).blades.get(targetBlade) != null) guiGraphics.pose()
                     .rotateAround(LightsaberTag.getOrCreate(stack).blades.get(targetBlade).emitterPos.q(), 0, (float) (hiltLength - emitterPos.y() - hiltLength / 2) * scale, 0);
             guiGraphics.pose().scale(scale, scale, scale);
             guiGraphics.pose().mulPoseMatrix(new Matrix4f().scaling(1.0F, -1.0F, 1.0F));
