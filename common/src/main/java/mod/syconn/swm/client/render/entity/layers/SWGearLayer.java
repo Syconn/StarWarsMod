a0d83a157dc3cac4720c74b9e8288719774db4f7
Syconn <49461127+Syconn@users.noreply.github.com>
2025-08-01 02:51:49 -0400

    Updated Side Slot Rendering for Lightsaber

--- common/src/main/java/mod/syconn/swm/client/render/entity/layers/SWGearLayer.java
+++ common/src/main/java/mod/syconn/swm/client/render/entity/layers/SWGearLayer.java
@@ -2,7 +2,9 @@ package mod.syconn.swm.client.render.entity.layers;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
+import mod.syconn.swm.features.lightsaber.item.LightsaberItem;
 import mod.syconn.swm.features.lightsaber.server.data.LightsaberTag;
+import mod.syconn.swm.utils.client.NodeVec3;
 import mod.syconn.swm.utils.interfaces.IEquipmentItem;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.player.AbstractClientPlayer;
@@ -30,10 +32,13 @@ public class SWGearLayer<P extends Player, M extends PlayerModel<P>> extends Ren
 
         var gear = player.swm$getSWGear();
         var stack = gear.getItemFromSlot(IEquipmentItem.SWEquipmentSlot.LIGHTSABER);
-        if (player instanceof AbstractClientPlayer clientPlayer && !stack.isEmpty()) {
-            poseStack.translate(clientPlayer.getMainArm() == HumanoidArm.RIGHT ? 0.3f : -0.3f, 1f, 0f);
-            if (LightsaberTag.getOrCreate(stack).model.getPath().equals("lightsaber/dark_saber")) poseStack.scale(0.5f, 0.5f, 0.5f);
-            poseStack.mulPose(Axis.YP.rotationDegrees(90f));
+        if (player instanceof AbstractClientPlayer clientPlayer && stack.getItem() instanceof LightsaberItem) {
+            final var emitterPos = LightsaberTag.getOrCreate(stack).blades.isEmpty() ? new NodeVec3() : LightsaberTag.getOrCreate(stack).blades.get(0).emitterPos;
+            final var hiltLength = LightsaberTag.getOrCreate(stack).hiltLength();
+
+            poseStack.translate((clientPlayer.getMainArm() == HumanoidArm.RIGHT ? 0.28f : -0.28f) - emitterPos.x(), 0.8f + (float) (hiltLength - emitterPos.y() - hiltLength / 2), -emitterPos.z());
+            if (player.isCrouching()) poseStack.translate(0f, 0f, 0.25f);
+            poseStack.mulPose(Axis.YP.rotationDegrees(180f));
             this.itemRenderer.renderStatic(LightsaberTag.getTemporary(stack, false), ItemDisplayContext.NONE, packedLight, OverlayTexture.NO_OVERLAY, poseStack, buffer, player.level(), player.getId());
         }
 
